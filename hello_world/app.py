4f9e8f5097f97da8a6cbf0f0d5de80d07d7c6249
ShigeruOda <42311356+shigeru-oda@users.noreply.github.com>
2023-02-17 22:22:31 +0900

    Update app.py

--- hello_world/app.py
+++ hello_world/app.py
@@ -1,9 +1,10 @@
-from fastapi.testclient import TestClient
-from hello_world.app import app
+from fastapi import FastAPI
+from mangum import Mangum
 
-client = TestClient(app)
+app = FastAPI()
 
-def test_root():
-    response = client.get("/hello")
-    assert response.status_code == 200
-    assert response.json() == {"message": "Hello SAM World"}
+@app.get("/hello", status_code=200)
+async def root():
+  return {"message": "Hello SAM World"}
+
+lambda_handler = Mangum(app)
